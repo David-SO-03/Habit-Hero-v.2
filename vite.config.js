e7b99ebea33130ccd9fbc2c7baef19e1e1c17425
David Sánchez <127771127+David-SO-03@users.noreply.github.com>
2026-01-19 11:36:20 +0000

    vite.config.js actualizado

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: false, // usamos tu manifest existente
+      includeAssets: ["icons/icon-192.png", "icons/icon-512.png"],
+    }),
+  ],
+});
